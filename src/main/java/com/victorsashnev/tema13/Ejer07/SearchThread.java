3eff261aaa120a1c444214d4cfd7a6bd95fed1d3
Victor <rusvictor@hotmail.es>
2023-05-25 12:07:43 +0200

    25/05/23

--- src/main/java/com/victorsashnev/tema13/Ejer07/SearchThread.java
+++ src/main/java/com/victorsashnev/tema13/Ejer07/SearchThread.java
@@ -1,5 +1,8 @@
 package com.victorsashnev.tema13.Ejer07;
 
+import java.time.Duration;
+import java.time.Instant;
+
 public class SearchThread extends Thread{
     private final int[] aray;
     private final int search;
@@ -14,9 +17,12 @@ public class SearchThread extends Thread{
     }
     @Override
     public void run(){
+        Instant inst1 = Instant.now();
         for(int i = begin; i < end;i++){
             if(aray[i] == search){
+                Instant inst2 = Instant.now();
                 System.out.println("Element found");
+                System.out.println("Elapsed Time: "+ Duration.between(inst1, inst2).toString());
             }
         }
     }
